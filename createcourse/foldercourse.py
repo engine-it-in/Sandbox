10d84d69c56ae428174776c9682966416f122908
engine_it <engineit@mail.ru>
2020-06-08 23:23:03 +0300

    Refactor structure of project

--- createcourse/foldercourse.py
+++ createcourse/foldercourse.py
@@ -2,12 +2,26 @@ import docx
 from docx import Document
 """Create structure of folder with files"""
 
+FILE = 0 # it's file with structure of folder
 
-def create_folder_courses(structura):
+structura = 0 # it's dictionary with name of folder
 
+def create_folder_courses(FILE):
+    """ ! """
+    structura = open_file(FILE)
+    create_folders(structura)
+    print('Success')
+
+
+def open_file(FILE):
+    """ ! """
+    pass
+
+
+def create_folders(structura):
+    """ ! """
+    pass
 
 
 if __name__ == '__main__':
-    structura = None
-    create_biblio(structura)
-
+    print("Compile :-) ")
