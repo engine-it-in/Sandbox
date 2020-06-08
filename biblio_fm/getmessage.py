bfea8a1106c0e791dd139efb3619dafe18994130
engine_it <engineit@mail.ru>
2020-06-09 00:02:48 +0300

    PEP 8

--- biblio_fm/getmessage.py
+++ biblio_fm/getmessage.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """Work with message from file"""
 
-import admindata as ad
 
 ADMIN_TEXT = 'admintext.py'
 
@@ -14,14 +13,14 @@ def dict_by_file(file):
     dic = dict()
     dao = open(file, 'r', encoding='utf-8')
     for item in dao:
-        key, *value = i.split(' = ')
+        key, *value = item.split(' = ')
         value = ''.join(value).replace("'", "").capitalize().strip() + ' '
         dic[key] = value
     return dic
 
 
 def usr_com(key):
-    """ Get user message from file
+    """ Get user message
     :param key: key from file with message
     :return: value of message
     """
@@ -32,14 +31,17 @@ def usr_com(key):
         return None
 
 
-def discover_separator(file):
-    open_file = open(file, 'r', encoding='utf-8')
-    bag = []
+def discover_separator(file_separator):
+    """ Compare possible separator from ADMIN_TEXT with reference
+    :param file_separator: references value of separators
+    :return: separator
+    """
+    open_file = open(ADMIN_TEXT, 'r', encoding='utf-8')
     try:
         for item in open_file:
             item = open_file.readline().split()
             item = item[1]
-            if item in ad.separator:
+            if item in file_separator:
                 break
             else:
                 continue
@@ -50,7 +52,10 @@ def discover_separator(file):
 
 
 def correct_exist_value(key):
-    """Check existing values"""
+    """Check existing values of key in dict
+    :param key: key of dict
+    :return: key or none
+    """
     dic = dict_by_file(ADMIN_TEXT)
     if key in set(dic.keys()):
         return key
@@ -62,6 +67,7 @@ def add_com_to_file(key, value):
     """ Add command and text to file (admin data)
     :param key: key - command
     :param value: value
+    :return: alert
     """
     dao = open(ADMIN_TEXT, 'a', encoding='utf-8')
     dao.write(key + ' = ' + "'" + (value) + "'" + '\n')
@@ -71,7 +77,8 @@ def add_com_to_file(key, value):
 
 def usr_com_with_variants(key):
     """ Transform usually command to command with variants
-    :param key: key
+    :param key: key of command
+    :return: complex answer with y/n
     """
     answer = usr_com(key) + usr_com('Var')
     return answer
@@ -80,6 +87,7 @@ def usr_com_with_variants(key):
 def add_new_message_by_key(key):
     """ Add new message in file by key
     :param key:
+    :return: nothing
     """
     if correct_exist_value(key) is None:
         value = 'y'
--- createcourse/foldercourse.py
+++ createcourse/foldercourse.py
@@ -7,21 +7,22 @@ FILE = 0 # it's file with structure of folder
 structura = 0 # it's dictionary with name of folder
 
 def create_folder_courses(FILE):
-    """ ! """
+    """ Create folder of courses
+    """
     structura = open_file(FILE)
     create_folders(structura)
     print('Success')
 
 
 def open_file(FILE):
-    """ ! """
-    pass
+    """ Open file structure and modify it in dict """
+    return structura
 
 
 def create_folders(structura):
-    """ ! """
-    pass
+    """ Create folders by dict  """
+    print('Success or Fail')
 
 
 if __name__ == '__main__':
-    print("Compile :-) ")
+    print("You are win. I'm compile :-) ")
