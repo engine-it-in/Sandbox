e48d47dc8f1c629ea51e6df3f54b2633dc5fc5a4
engine_it <engineit@mail.ru>
2020-06-08 23:15:15 +0300

    Refactor structure of project

---
+++ createcourse/foldercourse.py
@@ -0,0 +1,13 @@
+import docx
+from docx import Document
+"""Create structure of folder with files"""
+
+
+def create_folder_courses(structura):
+
+
+
+if __name__ == '__main__':
+    structura = None
+    create_biblio(structura)
+
